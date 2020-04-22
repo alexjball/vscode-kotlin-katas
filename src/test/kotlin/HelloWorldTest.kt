d31dfbb64bc80a6a26125370f8d19da157c8deb1
alexjball <8595776+alexjball@users.noreply.github.com>
2020-04-22 18:20:38 -0400

    Add Poker Hands instructions

--- src/test/kotlin/HelloWorldTest.kt
+++ src/test/kotlin/HelloWorldTest.kt
@@ -1,6 +1,18 @@
 import kotlin.test.assertEquals
 import org.junit.jupiter.api.Test
 
+/**
+ * Poker Hand Kata: http://codingdojo.org/kata/PokerHands/
+ * 
+ * Driver (D): The person that codes
+ * Navigator (N): The person that tells the driver what to do
+ * 5 minute rounds
+ * Roles rotate down and around
+ * 
+ * D
+ * N
+ */
+
 class HelloWorldTest {
     @Test
     fun `this finally fucking works`() {
